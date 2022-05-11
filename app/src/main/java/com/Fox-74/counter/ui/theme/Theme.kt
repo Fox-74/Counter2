d62fc5f12e0af0ce58aacb1bf2a9915b08fc867d
Fox-74 <80579921+Fox-74@users.noreply.github.com>
2022-05-11 09:01:57 +0500

    Update Theme.kt

--- app/src/main/java/com/Fox-74/counter/ui/theme/Theme.kt
+++ app/src/main/java/com/Fox-74/counter/ui/theme/Theme.kt
@@ -21,14 +21,6 @@ private val LightColorPalette = lightColors(
     secondary = Blue200,
     secondaryVariant = Blue700
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable

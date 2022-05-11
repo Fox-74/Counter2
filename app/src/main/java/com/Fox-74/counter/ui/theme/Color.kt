fad60ccf5b8166617b4a894cb43ffb3762cb3495
Fox-74 <80579921+Fox-74@users.noreply.github.com>
2022-05-11 09:01:14 +0500

    Update Color.kt

--- app/src/main/java/com/Fox-74/counter/ui/theme/Color.kt
+++ app/src/main/java/com/Fox-74/counter/ui/theme/Color.kt
@@ -2,8 +2,6 @@ package com.birhos.counter.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-//ref= https://materialui.co/colors
-//     https://material.io/design/color/the-color-system.html#color-theme-creation
 val Teal200 = Color(0xFF03DAC5)
 val Teal500 = Color(0xFF009688)
 val Teal700 = Color(0xFF00796B)
